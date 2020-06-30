877ac9104ec09d92cd8b233c801662f802a637ff
Caylem <caylemh@gmail.com>
2020-06-30 13:40:40 +0200

    Using Components & Using Function Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
     return ( 
         <div className = "App" >
-        Welcome to Note Junkie, Caylem!
+            <h1>Welcome to Note Junkie, Caylem!</h1>
+            <FirstComponent/>
+            <SecondComponent/>
+            <ThirdComponent/>
         </div>
     );
+}
+
+// Class Component
+class FirstComponent extends Component {
+    render() {
+        return ( 
+            <div className = "firstComponent" >
+                <h3>First Component</h3>
+            </div>
+        );
+    }
+}
+
+// Class Component
+class SecondComponent extends Component {
+    render() {
+        return ( 
+            <div className = "secondComponent" >
+                <h3>Second Component</h3>
+            </div>
+        );
+    }
+}
+
+// Class Component
+function ThirdComponent() {
+        return ( 
+            <div className = "thirdComponent" >
+                <h3>Third Component</h3>
+            </div>
+        );
 }
 
 export default App;
