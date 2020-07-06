25dc01262f3a5fbff2bb215cefc2920e1408a7f0
Caylem <caylemh@gmail.com>
2020-07-06 12:33:17 +0200

    Section 5: Working with & Enhancing the Spring Hello World App.

---
+++ rest-api/src/main/java/com/caylem/bootcamp/spring/restapi/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.caylem.bootcamp.spring.restapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Controller to handle REST Requests
+public class HelloWorldController {
+
+    @GetMapping(path="/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    //hello-world-bean
+    @GetMapping(path="/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
+}
