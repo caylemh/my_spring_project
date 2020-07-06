0a3fdb2c5d21d44f790aac8df9a196e5f01aa559
Caylem <caylemh@gmail.com>
2020-07-06 14:38:54 +0200

    Section 5: Working with Path variables

--- rest-api/src/main/java/com/caylem/bootcamp/spring/restapi/HelloWorldController.java
+++ rest-api/src/main/java/com/caylem/bootcamp/spring/restapi/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.caylem.bootcamp.spring.restapi;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // Controller to handle REST Requests
@@ -15,6 +16,13 @@ public class HelloWorldController {
     @GetMapping(path="/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    //hello world bean request with path parameter
+    // hello-world/path-variable/caylem
+    @GetMapping(path="/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathvariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
 }
