39f04641f8d9c351bfe59259613ce6620cb89a22
Caylem <caylemh@gmail.com>
2020-07-07 11:07:16 +0200

    Some updates to the RESTful API

--- rest-api/src/main/java/com/caylem/bootcamp/spring/restapi/HelloWorldController.java
+++ rest-api/src/main/java/com/caylem/bootcamp/spring/restapi/HelloWorldController.java
@@ -1,10 +1,12 @@
 package com.caylem.bootcamp.spring.restapi;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // Controller to handle REST Requests
+@CrossOrigin(origins="http://localhost:4200")
 public class HelloWorldController {
 
     @GetMapping(path="/hello-world")
@@ -15,14 +17,14 @@ public class HelloWorldController {
     //hello-world-bean
     @GetMapping(path="/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
-        return new HelloWorldBean("Hello World");
+        return new HelloWorldBean("Hello World Bean");
     }
 
     //hello world bean request with path parameter
     // hello-world/path-variable/caylem
     @GetMapping(path="/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
-        return new HelloWorldBean(String.format("Hello World, %s", name));
+        return new HelloWorldBean(String.format("Hello, %s", name.toUpperCase()));
     }
 
 }
