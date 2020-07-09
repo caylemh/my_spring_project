216e742b0dccb98d8381ffb428dabaf364ea20a1
Caylem <caylemh@gmail.com>
2020-07-09 14:12:30 +0200

    Section 6: Connect the Frontend to RESTful Service

--- frontend/src/components/todo/ListTodoComponent.jsx
+++ frontend/src/components/todo/ListTodoComponent.jsx
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import TodoDataService from '../../api/todo/TodoDataService';
+import AuthenticationService from './AuthenticationService';
 
 class ListTodoComponent extends Component {
     constructor(props) {
@@ -7,14 +9,26 @@ class ListTodoComponent extends Component {
         this.state = {
             todos : 
             [
-                {id: 1, descrip: 'Learn React', done: false, targetDate: new Date()},
-                {id: 2, descrip: 'Visit India', done: false, targetDate: new Date()},
-                {id: 3, descrip: 'Make a Full Stack Application', done: false, targetDate: new Date()},
-                {id: 4, descrip: 'Become a Full Stack Developer', done: false, targetDate: new Date()}
+                // {id: 1, desc: 'Learn React', done: false, targetDate: new Date()},
+                // {id: 2, desc: 'Visit India', done: false, targetDate: new Date()},
+                // {id: 3, desc: 'Make a Full Stack Application', done: false, targetDate: new Date()},
+                // {id: 4, desc: 'Become a Full Stack Developer', done: false, targetDate: new Date()}
             ]
             
         }
     }
+
+    componentDidMount() {
+        let username = AuthenticationService.getLoggedInUser();
+        TodoDataService.retrieveAllTodos(username)
+        .then(
+            response => {
+                //console.log(response);
+                this.setState({ todos: response.data });
+            }
+        )
+    }
+
     render() {
         return (
             <div>
@@ -33,7 +47,7 @@ class ListTodoComponent extends Component {
                                 this.state.todos.map ( 
                                     todo =>
                                         <tr key={todo.id}>
-                                            <td>{todo.descrip}</td>
+                                            <td>{todo.desc}</td>
                                             <td>{todo.targetDate.toString()}</td>
                                             <td>{todo.done.toString()}</td>
                                         </tr>
