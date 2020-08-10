bc724c02e75aec13308166cd88371c879479f194
Caylem <caylemh@gmail.com>
2020-08-10 23:21:02 +0200

    Section 9: Connecting React Frontend with JPA

--- frontend/src/api/todo/TodoDataService.js
+++ frontend/src/api/todo/TodoDataService.js
@@ -1,30 +1,30 @@
 import axios from 'axios';
-import {API_URL} from '../../Constants'
+import {API_URL, JPA_API_URL} from '../../Constants'
 
 class TodoDataService {
     
     retrieveAllTodos(name) {
-        return axios.get(`${API_URL}/users/${name}/todos`);
+        return axios.get(`${JPA_API_URL}/users/${name}/todos`);
         //console.log('executed service!');
     }
 
     retrieveTodo(name, id) {
-        return axios.get(`${API_URL}/users/${name}/todos/${id}`);
+        return axios.get(`${JPA_API_URL}/users/${name}/todos/${id}`);
         //console.log('executed service!');
     }
 
    deleteTodo(name, id) {
-        return axios.delete(`${API_URL}/users/${name}/todos/${id}`);
+        return axios.delete(`${JPA_API_URL}/users/${name}/todos/${id}`);
         // console.log('executed service!');
     }
 
     updateTodo(name, id, todo) {
-        return axios.put(`${API_URL}/users/${name}/todos/${id}`, todo);
+        return axios.put(`${JPA_API_URL}/users/${name}/todos/${id}`, todo);
         // console.log('executed service!');
     }
 
     createTodo(name, todo) {
-        return axios.post(`${API_URL}/users/${name}/todos/`, todo);
+        return axios.post(`${JPA_API_URL}/users/${name}/todos/`, todo);
         // console.log('executed service!');
     }
 
